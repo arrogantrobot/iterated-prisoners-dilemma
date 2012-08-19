72933263bfe68b560a4169439f2ad32c702f03f7
Rob Long <robert.l.long@gmail.com>
2012-08-19 15:37:13 -0500

    removed class files

--- iterated-prisoners-dilemma/src/main/java/com/farawaylabs/ipd/Tournament.java
+++ iterated-prisoners-dilemma/src/main/java/com/farawaylabs/ipd/Tournament.java
@@ -14,7 +14,9 @@ public class Tournament {
     }
 
     public void play()  {
-        for(int i = 0; i < 200; i++) {
+        System.out.println("P1\tP2");
+
+        for(int i = 0; i < 2000; i++) {
             playRound(0,1);
         }
         displayScores();
@@ -25,18 +27,23 @@ public class Tournament {
         boolean player2 = players.get(p2).getPlay();
         players.get(p1).result(player2);
         players.get(p2).result(player1);
+        //System.out.println(Boolean.valueOf(player1).toString()+"\t"+Boolean.valueOf(player2).toString());
         if (player1 && player2)  {
             players.get(p1).incrementScore(REWARD);
             players.get(p2).incrementScore(REWARD);
+            //System.out.println("Cooperated");
         } else if (!player1 && !player2) {
             players.get(p1).incrementScore(PUNISHMENT);
             players.get(p2).incrementScore(PUNISHMENT);
+            //System.out.println("Dual defection");
         } else if (player1) {
             players.get(p1).incrementScore(SUCKER);
             players.get(p2).incrementScore(TEMPTATION);
+            //System.out.println(players.get(p1).getPlayerName()+" was the sucker.");
         } else {
             players.get(p1).incrementScore(TEMPTATION);
             players.get(p2).incrementScore(SUCKER);
+            //System.out.println(players.get(p2).getPlayerName()+" was the sucker.");
         }
     }
 
